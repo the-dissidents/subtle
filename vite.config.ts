3089f5ce615c1febd5d85f9d0cf0fe8e0713d65d
the-dissidents <the_dissidents@outlook.com>
2025-07-05 04:54:34 +0800

    update npm packages and fix audit issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
